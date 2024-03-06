fe96dd36c3d9115b34288dbbf0598219a0331d44
dejavuho2 <155792560+dejavuho2@users.noreply.github.com>
2024-03-06 07:28:05 +0700

    commit

---
+++ InputString/src/InputString/Main.java
@@ -0,0 +1,26 @@
+package InputString;
+
+import models.InputData;
+import view.InputDataView;
+import controller.InputDataController;
+
+import java.util.Scanner;
+
+public class Main {
+    private final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===== Analysis String program ====");
+        System.out.printf("Input string: ");
+        String input = sc.nextLine();
+        InputData inputData = new InputData(input);
+        InputDataView inputDataView = new InputDataView(inputData);
+        InputDataController inputDataController = new InputDataController(inputData, inputDataView);
+        System.out.println(" ");
+        System.out.println("Processing...");
+        System.out.println(" ");
+        inputDataController.handleRequest(input);
+    }
+
+}
